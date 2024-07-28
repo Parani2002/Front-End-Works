827188ccfe83dde207e19638aa6940f2e2f44848
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-07-28 21:25:30 +0530

    face.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import { React, useState } from "react";
+import React from "react";
+import Face from "./components/Face";
 
 function App() {
-  const cities = ["Jaffna", "Colombo", "Vavuniya", "Kilinochi"];
-  const [name, setName] = useState("");
-  const [value, setValue] = useState(0)
-  const updateCityName = () => {
-    setName(cities[value])
-    setValue(value+1)
-    if(value >= cities.length){
-      setValue(0)
-    }
-
-  }
+  
 
   return (
-    <div>
-      <button onClick={updateCityName} className="border bg-violet-600 px-4 py-2 rounded-md">Click Me</button>
-      {cities.map((city) => (
-        <div>
-          <h1>{city}</h1>
-        </div>
-      ))}
-
-      <h1>City Name is : {name}</h1>
+    <div className="w-full h-screen flex justify-center align-middle p-16">
+      <Face />
+      
     </div>
 
   )
-
 }
+
+
 
 export default App;
---
+++ src/components/Face.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Face = () => {
+  return (
+    <div className='bg-red-600 w-1/2 h-1/2 rounded-full'>
+        <div className='flex justify-around mt-8'>
+            <span className='bg-green-700 px-4 py-2  rounded-full'>EYE</span>
+            <span className='bg-green-700 px-4 py-2  rounded-full'>EYE</span>
+        </div>
+
+    </div>
+  )
+}
+
+export default Face
