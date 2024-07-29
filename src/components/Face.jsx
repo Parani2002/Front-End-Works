e4f6999e63ef247c218bbd15a4b3abcbe87c39e4
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-07-29 20:14:19 +0530

    nose and mouth centered

--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -7,13 +7,21 @@ const Face = () => {
         <span className="bg-green-700 p-8  rounded-full border border-r-2">EYE</span>
         <span className="bg-green-700 p-8  rounded-full border border-r-2">EYE</span>
       </div>
+      <div className="flex justify-center">
       <div className="bg-blue-700 text-center mt-8 rounded-full w-1/4 flex justify-center px-4 py-2">
         NOSE
       </div>
-      <div className="flex flex-col align-middle justify-center text-center mt-8 rounded-full w-1/2 px-4 py-2">
-        <span className="bg-violet-500">UPPER LIP</span>
-        <span className="bg-violet-800">LOWER LIP</span>
+
       </div>
+      
+      <div className="flex justify-center">
+      <div className="flex flex-col align-middle justify-center text-center mt-8 rounded-full w-1/2 px-4 py-2 space-y-1">
+        <span className="bg-violet-500 rounded-tr-full rounded-tl-full">UPPER LIP</span>
+        <span className="bg-violet-800 rounded-bl-full rounded-br-full">LOWER LIP</span>
+      </div>
+
+      </div>
+      
     </div>
   );
 };
