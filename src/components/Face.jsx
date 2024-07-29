15e06e2d07a16f5b6e3f79efcf52bb56507c1059
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-07-29 21:35:50 +0530

    update on face.jsx

--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Face = () => {
   return (
-    <div className="bg-red-600 flex flex-col w-1/2 h-1/2 rounded-full p-8">
+    <div className="bg-red-600 flex flex-col w-1/2 h-3/4 rounded-full p-8">
       <div className="flex justify-around mt-8">
         <span className="bg-green-700 p-8  rounded-full border border-r-2">EYE</span>
         <span className="bg-green-700 p-8  rounded-full border border-r-2">EYE</span>
