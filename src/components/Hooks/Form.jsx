58b5026c5a3246d3dc2b0149de43e36670942ee0
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2024-11-30 20:18:33 +0530

    dynamic array implemented

--- src/components/Hooks/Form.jsx
+++ src/components/Hooks/Form.jsx
@@ -1,52 +1,99 @@
-import { React } from "react";
-import { useForm } from "react-hook-form";
+import { React, useEffect } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
 
 const Form = () => {
-  const {
-    register,
-    handleSubmit,
-    formState,
-    watch
-  } = useForm();
+  const { register, handleSubmit, formState, watch, control } = useForm({
+    defaultValues:{
+      name:"",
+      emailaddress:"",
+      address:""
+    }
+  });
+  const { fields,append, remove } = useFieldArray({
+    control,
+    name:"address"
+  });
 
-  const {errors} = formState;
-//   console.log(errors);
-//   console.log(formState);
+  const { errors } = formState;
+    console.log(fields);
+  //   console.log(formState);
 
-  const onSubmit = data => console.log(data);
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
       <h1>Registration</h1>
-      <form action="" method="post" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="name">Name</label><br />
-        <input type="text" name="name" id="name" placeholder="Enter your name" {...register('name',{
-            required:{
-                value:true,
-                message:"User name is required..."
-            }
-        })}/>
+      <form
+        action=""
+        method="post"
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label htmlFor="name">Name</label>
+        <br />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Enter your name"
+          {...register("name", {
+            required: {
+              value: true,
+              message: "User name is required...",
+            },
+          })}
+        />
         <br />
         <p>{errors.name?.message}</p>
         <br />
-
-
-        <label htmlFor="emailaddress">Email Address</label><br />
-        <input type="email" name="emailaddress" id="emailaddress" placeholder="Enter your email address" {...register('emailaddress',{
-            required:{
-                value:true,
-                message:"Email is required"
+        <label htmlFor="emailaddress">Email Address</label>
+        <br />
+        <input
+          type="email"
+          name="emailaddress"
+          id="emailaddress"
+          placeholder="Enter your email address"
+          {...register("emailaddress", {
+            required: {
+              value: true,
+              message: "Email is required",
             },
-            pattern:{
-                value:"/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/",
-                message:"Invalid email format"
-            }})}/>
-            <br />
-            <p>{errors.emailaddress?.message}</p>
-            <br />
-
-        
-
-        <label htmlFor="password">Password</label><br />
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
+        <br />
+        <p>{errors.emailaddress?.message}</p>
+        <br />
+        <label htmlFor="name">Address</label> <br />
+        {fields.map((field, index) => (
+          <div>
+          <input key={field.id}
+            type="text"
+            name="address"
+            id="address"
+            placeholder="Enter your address"
+            {...register(`address.${index}.city`, {
+              required: {
+                value: true,
+                message: "Address is required...",
+              },
+            })}
+          />
+           <button type="button" onClick={useEffect()}>Delete</button>
+          </div>
+        ))}
+        <p>{errors.address?.message}</p>
+        <br />
+        <button
+        type="button"
+        onClick={() => append({ address:""})}
+      >
+        append
+      </button>
+       
+        {/* <label htmlFor="password">Password</label><br />
         <input type="password" name="password" id="password" {...register('password',{
             required:{
                 value:true,
@@ -67,11 +114,10 @@ const Form = () => {
                     return "Password doesnot match";
                 }
             } 
-        })}/>
+        })}/> */}
         <br />
         <p>{errors.confirm_password?.message}</p>
         <br />
-
         <input type="submit" value="submit" />
       </form>
     </div>
